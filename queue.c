b5766acfb6b531ef1af9d2a9e75acc51e209f9d9
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2023-01-24 21:53:13 -0500

    Created qput and qget

--- queue.c
+++ queue.c
@@ -33,9 +33,19 @@ static internalQueue_t* makeQueue(void) {
 }
 
 // Frees memory for internalQueue_t
-static uint32_t removeQueue(internalQueue_t* queue) {
+static void removeQueue(internalQueue_t* queue) {
     free(queue);
-    return 1;
+}
+
+static queueItem_t* makeQueueItem(void *itemPointer) {
+    queueItem_t *ip = (queueItem_t*) malloc(sizeof(queueItem_t));
+    ip->next = NULL;
+    ip->item = itemPointer;
+    return ip;
+}
+
+static void removeQueueItem(queueItem_t *p) {
+    free(p);
 }
 
 // Creates an empty queue and gives pointer to user
@@ -44,7 +54,62 @@ queue_t* qopen(void) {
     return (queue_t*) queue;
 }
 
-// Closes a queue that the user has
+// Closes a queue that the user has opened
+// TODO: Deallocate queue elements as well
 void qclose(queue_t *qp) {
     removeQueue((internalQueue_t*) qp);
 }
+
+// Puts an item in the queue
+int32_t qput(queue_t *qp, void *elementp) {
+
+    // Convert user pointer to internal pointer
+    internalQueue_t *queue = (internalQueue_t*) qp;
+    
+    // Create queueItem from item
+    queueItem_t* item = makeQueueItem(elementp);
+
+    // Queue is empty
+    if (queue->front == NULL) {
+        queue->front = item;
+        queue->back = item;
+        return 0;
+    }
+
+    // Queue is not empty
+    else {
+        queueItem_t* currentBack = queue->back;
+        currentBack->next = item;
+        queue->back = item;
+        return 0;
+    }
+}
+
+// Gets the first element in the queue and returns a pointer to it. Returns 
+// NULL if queue is empty
+void *qget(queue_t *qp) {
+    // Convert user pointer to internal pointer
+    internalQueue_t *queue = (internalQueue_t*) qp;
+    
+    // Queue is empty
+    if (queue->front == NULL) 
+        return NULL;
+
+    // Get item at front of queue and make next item the front of the queue
+    queueItem_t *front = queue->front;
+    queue->front = front->next;
+
+    // Queue is now empty, so fix the back
+    if (queue->front == NULL)
+        queue->back = NULL;
+
+    // Extract item pointer from queueItem
+    void *itemPointer = front->item;
+
+    // Handle deallocation of memory
+    removeQueueItem(front);
+
+    return itemPointer;
+}
+
+
